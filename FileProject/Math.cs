4723e4f692404884d51558cd084c6319aa32508f
Nikita <Nikita@TheVer4.ru>
2021-03-06 15:26:23 +0500

    Added Math.cs

---
+++ FileProject/Math.cs
@@ -0,0 +1,14 @@
+﻿namespace FileProject {
+    public class Math {
+        
+        static int Add(int a, int b) {
+            return a + b;
+        }
+        
+        
+        static int Sub(int a, int b) {
+            return a - b;
+        }
+        
+    }
+}
--- FileProject/Program.cs
+++ FileProject/Program.cs
@@ -3,14 +3,5 @@
         static void Main(string[] args) {
             
         }
-
-        static int Add(int a, int b) {
-            return a + b;
-        }
-        
-        
-        static int Sub(int a, int b) {
-            return a - b;
-        }
     }
 }
