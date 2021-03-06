908b0494dcf09a1fe1ee17d729d4188870ea2252
Nikita <Nikita@TheVer4.ru>
2021-03-06 15:30:57 +0500

    Added Geometry.cs

---
+++ FileProject/Geometry.cs
@@ -0,0 +1,16 @@
+﻿namespace FileProject {
+    public class Geometry {
+
+        public static bool existsTriangle(int a, int b, int c) {
+            int count = 0;
+            if (a + b > c)
+                count++;
+            if (a + c > b)
+                count++;
+            if (b + c > a)
+                count++;
+            return count == 3;
+        }
+        
+    }
+}
